beae08f5c65a8a986afd3a416992042b2b6c4caa
weiranyi <88951231@qq.com>
2022-01-20 19:09:47 +0800

    JSONP

--- frank-com/public/frank.js
+++ frank-com/public/frank.js
@@ -1,3 +1,9 @@
+window.xxx = (data)=>{console.log(data)} // 成回调函数了
+const script = document.createElement('script')
+script.src = "http://qq.com:8888/friends.js"
+document.body.appendChild(script)
+/**
+ * CORS 测试代码
 const request = new XMLHttpRequest();
 request.open('GET','http://qq.com:8888/friends.json')
 request.onreadystatechange = ()=>{
@@ -6,3 +12,4 @@ request.onreadystatechange = ()=>{
     }
 }
 request.send()
+*/
--- qq-com/server.js
+++ qq-com/server.js
@@ -36,16 +36,16 @@ var server = http.createServer(function(request, response) {
         response.statusCode = 200;
         response.setHeader("Content-Type", "text/json;charset=utf-8");
         // CORS
-        response.setHeader("Access-Control-Allow-Origin", "http://frank.com:9999");
+        //response.setHeader("Access-Control-Allow-Origin", "http://frank.com:9999");
         response.write(fs.readFileSync("./public/friends.json"));
         response.end();
     } else if (path === "/friends.js") {
-        if (request.headers["referer"].indexOf("http://frank.com:9990") === 0) {
+        if (request.headers["referer"].indexOf("http://frank.com:9999") === 0) {
             response.statusCode = 200;
             response.setHeader("Content-Type", "text/javascript;charset=utf-8");
-            const string = `window['{{xxx}}']({{data}}) `
+            const string = fs.readFileSync("./public/friends.js").toString();
             const data = fs.readFileSync("./public/friends.json").toString();
-            const string2 = string.replace("{{data}}", data).replace('{{xxx}}', query.callback);
+            const string2 = string.replace("{data}", data).replace('{{xxx}}', query.callback);
             response.write(string2);
             response.end();
         } else {
