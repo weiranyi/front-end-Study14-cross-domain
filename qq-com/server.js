8201e0e663be76f3bdb58d4506bccf84ef8d4408
weiranyi <88951231@qq.com>
2022-01-20 18:42:18 +0800

    CORS

--- qq-com/server.js
+++ qq-com/server.js
@@ -35,6 +35,7 @@ var server = http.createServer(function(request, response) {
     } else if (path === "/friends.json") {
         response.statusCode = 200;
         response.setHeader("Content-Type", "text/json;charset=utf-8");
+        // CORS
         response.setHeader("Access-Control-Allow-Origin", "http://frank.com:9999");
         response.write(fs.readFileSync("./public/friends.json"));
         response.end();
